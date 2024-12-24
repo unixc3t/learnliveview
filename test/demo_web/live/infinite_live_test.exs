b5e3e8dd53d092e4146b1932bc472b7439625f46
rudy <c3tc3tc3t@126.com>
2024-12-24 20:38:18 +0800

    test-hooks

---
+++ test/demo_web/live/infinite_live_test.exs
@@ -0,0 +1,34 @@
+defmodule DemoWeb.InfiniteLiveTest do
+  use DemoWeb.ConnCase, async: true
+  import Phoenix.LiveViewTest
+
+  test "renders more orders when user scrolls to bottom", %{conn: conn} do
+    create_orders()
+
+    {:ok, view, _html} = live(conn, "/infinite")
+
+    assert render(view) |> number_of_orders() == 10
+
+
+    view
+    |> element("#footer")
+    |> render_hook("load-more", %{})
+
+
+    assert render(view) |> number_of_orders() == 11
+  end
+
+  defp number_of_orders(html) do
+    html |> :binary.matches("Test Pizza") |> length()
+  end
+
+  defp create_orders do
+    for i <- 1..11 do
+      {:ok, _order} =
+        Demo.PizzaOrders.create_pizza_order(%{
+          username: "Test User #{i}",
+          pizza: "Test Pizza #{i}"
+        })
+    end
+  end
+end
