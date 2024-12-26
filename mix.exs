adfa7d85e49f424a651c9692c3deb5dd710b917d
rudy <c3tc3tc3t@126.com>
2024-12-26 17:02:14 +0800

    add auth

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Demo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
       {:credo, "~> 1.7"},
